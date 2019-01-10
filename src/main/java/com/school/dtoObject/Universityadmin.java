ba8cebb2f17236f5a62f8175e957154f108ea561
young <502989211@qq.com>
2019-01-10 21:39:30 +0800

    添加备注 添加了学院信息dto类

--- src/main/java/com/school/dtoObject/Universityadmin.java
+++ src/main/java/com/school/dtoObject/Universityadmin.java
@@ -17,19 +17,19 @@ public class Universityadmin {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Integer UAdmId;
-
+    /** 管理员登录名     */
     private String UAdmUsername;
-
+    /** 管理员密码     */
     private String UAdmPassword;
-
+    /** 管理员名字     */
     private String UAdmName;
-
+    /** 管理员头像地址     */
     private String UAdmAvater;
-
+    /** 0表示学校管理员     */
     private Integer Identity=0;
-
+    /** 创建时间     */
     private Date CreateTime;
-
+    /** 更新信息时间     */
     private Date UpdateTime;
 
 
