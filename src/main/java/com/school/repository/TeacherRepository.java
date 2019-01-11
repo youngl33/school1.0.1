2dc89d13fb4a326b0106f0f15158cea0aa5568e4
yry <928219339@qq.com>
2019-01-11 21:01:18 +0800

    创建了TeahcerService 通过了测试

--- src/main/java/com/school/repository/TeacherRepository.java
+++ src/main/java/com/school/repository/TeacherRepository.java
@@ -2,9 +2,8 @@ package com.school.repository;
 
 import com.school.dtoObject.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface TeacherRepository extends JpaRepository<Teacher,String> {
-   /* public List<Teacher> findByTeacherName(String TeacherName);*/
+    public List<Teacher> findByTeacherName(String TeacherName);
 }
--- src/test/java/com/school/repository/TeacherRepositoryTest.java
+++ src/test/java/com/school/repository/TeacherRepositoryTest.java
@@ -8,13 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.print.attribute.standard.MediaSize;
 import java.text.ParseException;
-import java.util.Date;
-import java.util.List;
 
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
