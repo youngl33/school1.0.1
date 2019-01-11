9dfb2207c8082b642112f23bce865a9127ac4542
young <502989211@qq.com>
2019-01-11 17:33:13 +0800

    Create Academyinfo.java
    创建了学院信息dto类

---
+++ src/main/java/com/school/dtoObject/Academyinfo.java
@@ -0,0 +1,30 @@
+package com.school.dtoObject;
+
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+
+@DynamicUpdate
+@Data
+@Entity
+
+public class Academyinfo {
+
+    @Id
+    private String AInfoId;
+    /** 学院名字1 */
+    private String AInfoName;
+    /** 学院描述信息 */
+    private String AInfoDescription;
+    /** 创建时间 */
+    private Timestamp CreateTime;
+    /** 更新时间 */
+    private Timestamp UpdateTime;
+
+
+}
