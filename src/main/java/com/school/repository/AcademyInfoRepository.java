27117d7b4031fa95ff2b60ec2ef9abcf61131b25
young <502989211@qq.com>
2019-01-11 16:56:40 +0800

    引用包
    修改了未引用包的问题

--- src/main/java/com/school/repository/AcademyInfoRepository.java
+++ src/main/java/com/school/repository/AcademyInfoRepository.java
@@ -1,5 +1,6 @@
 package com.school.repository;
 
+import com.school.dtoObject.Academyinfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AcademyInfoRepository extends JpaRepository<Academyinfo,Integer> {
