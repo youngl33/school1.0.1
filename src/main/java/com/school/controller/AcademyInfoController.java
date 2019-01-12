4714363bc86e2f35023ec8219e061f0b8781198f
young <502989211@qq.com>
2019-01-12 16:01:22 +0800

    新增模板功能

---
+++ src/main/java/com/school/controller/AcademyInfoController.java
@@ -0,0 +1,21 @@
+package com.school.controller;
+
+
+import com.school.service.AcademyInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/academy")
+public class AcademyInfoController {
+
+    @Autowired
+    private AcademyInfoService academyInfoService;
+
+    @GetMapping("/list")
+    public String findAll(){
+        return "teacher/index";
+    }
+}
