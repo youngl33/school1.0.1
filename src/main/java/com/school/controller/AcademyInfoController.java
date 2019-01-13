829d2972115f6efa6d0519e6ca4b6ab5f0fc7a00
young <502989211@qq.com>
2019-01-13 16:20:03 +0800

    修改web模板

--- src/main/java/com/school/controller/AcademyInfoController.java
+++ src/main/java/com/school/controller/AcademyInfoController.java
@@ -1,11 +1,19 @@
 package com.school.controller;
 
 
+import com.school.dtoObject.AcademyInfo;
 import com.school.service.AcademyInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/academy")
@@ -15,8 +23,12 @@ public class AcademyInfoController {
     private AcademyInfoService academyInfoService;
 
     @GetMapping("/list")
-    public String findAll(){
-        return "teacher/index";
-
+    public ModelAndView findAll(@RequestParam(value="page",defaultValue = "0")Integer page,
+                                Map<String,Object> map){
+        PageRequest request = new PageRequest(page,15);
+        Page<AcademyInfo> academyInfoPage = academyInfoService.findAll(request);
+        List<AcademyInfo> academyInfoList = academyInfoPage.getContent();
+        map.put("academyInfos",academyInfoList);
+        return new ModelAndView("academy/index");
     }
 }
